b13ee0bb83f7a8b58a59b30eacdc263d830a395a
jakeolase <mail.jakeolase@gmail.com>
2023-12-29 20:21:07 +0800

    Added tech logo color on hover

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var cursor = document.querySelector('.blob');
 
-document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-});
+function moveCursor(e) {
+  var x, y;
+
+  if (e.type === 'mousemove') {
+    x = e.clientX;
+    y = e.clientY;
+  } else if (e.type === 'touchmove') {
+    x = e.touches[0].clientX;
+    y = e.touches[0].clientY;
+  }
+
+  cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
+}
+
+document.addEventListener('mousemove', moveCursor);
+document.addEventListener('touchmove', moveCursor);
+
