be036b40e58e2cff4a9ff7eba6f7747661c24eb8
jakeolase <mail.jakeolase@gmail.com>
2023-12-30 17:16:47 +0800

    Added projects div emphasis when div are visible in the viewport

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function moveCursor(e) {
 document.addEventListener('mousemove', moveCursor);
 document.addEventListener('touchmove', moveCursor);
 
+document.addEventListener("DOMContentLoaded", function() {
+  const projectImages = document.querySelectorAll(".project_img");
+
+  const options = {
+    threshold: 0.8,
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+
+  projectImages.forEach(img => {
+    observer.observe(img);
+  });
+
+  function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.filter = "none";
+        entry.target.style.transform = "scale(1.0)";
+      } else {
+        entry.target.style.filter = "grayscale()";
+        entry.target.style.transform = "scale(0.95)";
+      }
+    });
+  }
+});
